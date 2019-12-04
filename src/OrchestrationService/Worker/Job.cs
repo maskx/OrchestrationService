f1274fb8faf6ed393d66021ba16dfc31693f12ea
maskx <maskx@outlook.com>
2019-12-04 15:24:57 +0800

    add AspNetCore2.1 project

--- src/OrchestrationService/Worker/Job.cs
+++ src/OrchestrationService/Worker/Job.cs
@@ -1,7 +1,4 @@
-﻿using maskx.OrchestrationService.OrchestrationCreator;
-using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System;
 
 namespace maskx.OrchestrationService.Worker
 {
