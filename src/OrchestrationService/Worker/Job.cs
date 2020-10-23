cfe6d7efdf05f1713a47ecdd60e35f4791711f69
maskx <maskx@outlook.com>
2020-10-23 15:26:25 +0800

    Job.Input type string->object

--- src/OrchestrationService/Worker/Job.cs
+++ src/OrchestrationService/Worker/Job.cs
@@ -5,7 +5,7 @@ namespace maskx.OrchestrationService.Worker
     public class Job
     {
         public string InstanceId { get; set; }
-        public string Input { get; set; }
+        public object Input { get; set; }
         public string CreateBy { get; set; }
         public DateTime CreateTime { get; set; }
         public DateTime LastUpdateTime { get; set; }
